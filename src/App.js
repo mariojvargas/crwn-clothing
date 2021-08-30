54f17e0c3dadc6b778b15be33dd14204b23c426e
Mario Vargas <mvargas@nexient.com>
2021-08-30 10:52:25 -0400

    Add placeholders for Homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
 
 function App() {
-  return <div>Hello, CRWN Clothing</div>;
+  return <HomePage />;
 }
 
 export default App;
