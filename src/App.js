28b7102419f8aed2145db8b9cd5eb00cad57ff4c
Mario Vargas <mvargas@nexient.com>
2021-08-31 10:39:36 -0400

    Wrap MenuItem with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
-function App() {
-  return <HomePage />;
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+function App(props) {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
