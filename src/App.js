1a53d52d8d9dcb5e255b23601fec85e7e032e01e
Mario Vargas <mvargas@nexient.com>
2021-08-31 12:05:56 -0400

    Add collection preview to Shop page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App(props) {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
