b88d6873f867d80b249cd82f2adcea946dda064e
Mario Vargas <mvargas@nexient.com>
2021-09-08 15:27:23 -0400

    Add ability to change item quantity or clear item from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,8 @@
+export const getCartItemById = (cartItems, cartItemId) =>
+  cartItems.find((item) => item.id === cartItemId);
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find((item) => item.id === cartItemToAdd.id);
+  const existingCartItem = getCartItemById(cartItems, cartItemToAdd.id);
 
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
@@ -8,4 +11,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = getCartItemById(cartItems, cartItemToRemove.id);
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
