25fdf0356b5d666f18bcee28c10848ef3108da47
Mario Vargas <mvargas@nexient.com>
2021-09-09 09:28:14 -0400

    Move shop collection data to Redux store

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,5 @@
+import { createSelector } from 'reselect';
+
+const selectShop = ({ shop }) => shop;
+
+export const selectCollections = createSelector(selectShop, ({ collections }) => collections);
