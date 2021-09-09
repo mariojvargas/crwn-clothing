1066f9ca84ea9a05809bc0acc24d53e907cf77a7
Mario Vargas <mvargas@nexient.com>
2021-09-09 16:33:36 -0400

    Normalize collections in shop data file

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,21 +1,15 @@
 import memoize from 'lodash.memoize';
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = ({ shop }) => shop;
 
 export const selectCollections = createSelector(selectShop, ({ collections }) => collections);
 
 // This selector becomes a curried function
 export const selectCollection = memoize((collectionName) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find((c) => c.id === COLLECTION_ID_MAP[collectionName])
-  )
+  createSelector([selectCollections], (collections) => collections[collectionName])
+);
+
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) =>
+  Object.keys(collections).map((key) => collections[key])
 );
