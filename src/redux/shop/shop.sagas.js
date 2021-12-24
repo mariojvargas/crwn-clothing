eba2d74f1b4bb852e1c0f3906c34628b7f22bad1
Mario Vargas <angstrey@hotmail.com>
2021-12-23 19:11:38 -0500

    Add root shop sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,21 +1,23 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery, all } from 'redux-saga/effects';
 import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
 import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop.actions';
 import ShopActionTypes from './shop.types';
 
-export function* fetchCollectionsAsync() {
-  try {
-    const collectionRef = firestore.collection('collections');
-
-    const snapshop = yield collectionRef.get();
-    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshop);
-
-    yield put(fetchCollectionsSuccess(collectionsMap));
-  } catch (error) {
-    yield put(fetchCollectionsFailure, error.message);
-  }
+export function* shopSagas() {
+  yield all([call(onFetchCollectionsStart)]);
 }
 
-export function* fetchCollectionsStart() {
-  yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+function* onFetchCollectionsStart() {
+  yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, function* fetchCollectionsAsync() {
+    try {
+      const collectionRef = firestore.collection('collections');
+
+      const snapshop = yield collectionRef.get();
+      const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshop);
+
+      yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+      yield put(fetchCollectionsFailure, error.message);
+    }
+  });
 }
