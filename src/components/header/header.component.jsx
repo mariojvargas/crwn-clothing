91e990bd66e78d666aa240d09b715a7338c9467b
Mario Vargas <angstrey@hotmail.com>
2021-12-27 15:55:05 -0500

    Refactor Header component with hooks

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { useDispatch, useSelector } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { signOutStart } from '../../redux/user/user.actions';
@@ -8,35 +7,34 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import CartIcon from '../cart-icon/cart-icon.component';
 import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer } from './header.styles';
 
-const Header = ({ currentUser, cartHidden, signOut }) => (
-  <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo className="logo" />
-    </LogoContainer>
+const Header = () => {
+  const dispatch = useDispatch();
+  const signOut = () => dispatch(signOutStart());
 
-    <OptionsContainer>
-      <OptionLink to="/shop">Shop</OptionLink>
-      <OptionLink to="/shop">Contact</OptionLink>
-      {currentUser ? (
-        <OptionLink as="div" onClick={() => signOut()}>
-          Sign Out
-        </OptionLink>
-      ) : (
-        <OptionLink to="/signin">Sign In</OptionLink>
-      )}
-      <CartIcon />
-    </OptionsContainer>
-    {cartHidden ? null : <CartDropdown />}
-  </HeaderContainer>
-);
+  const currentUser = useSelector(selectCurrentUser);
+  const cartHidden = useSelector(selectCartHidden);
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  cartHidden: selectCartHidden,
-});
+  return (
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo className="logo" />
+      </LogoContainer>
 
-const mapDispatchToProps = (dispatch) => ({
-  signOut: () => dispatch(signOutStart()),
-});
+      <OptionsContainer>
+        <OptionLink to="/shop">Shop</OptionLink>
+        <OptionLink to="/shop">Contact</OptionLink>
+        {currentUser ? (
+          <OptionLink as="div" onClick={signOut}>
+            Sign Out
+          </OptionLink>
+        ) : (
+          <OptionLink to="/signin">Sign In</OptionLink>
+        )}
+        <CartIcon />
+      </OptionsContainer>
+      {cartHidden ? null : <CartDropdown />}
+    </HeaderContainer>
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
